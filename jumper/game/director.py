560518a396a02b3a2664011d56e6540961f1c8b1
Conner Haar <77696186+Connerhaar@users.noreply.github.com>
2021-02-05 19:02:55 -0700

    Fixed some errors in director

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -5,15 +5,18 @@ from game.word_bank import Word_Bank
 
 class Director:
     def __init__(self):
-        self.console = Console('game.wordlist.10000.txt')
         self.correct = True
         self.keep_playing = True
+        self.console = Console()
+        self.jumper = Jumper()
+        self.word_bank = Word_Bank()
+
 
     def start_game(self):
-        Word_Bank.load_list()
-        Word_Bank.choose_word()
-        Jumper.create_parachute()
-        Word_Bank.get_blank()
+        self.word_bank.load_list("wordlist.txt")
+        self.word_bank.choose_word()
+        self.jumper.create_parachute()
+        self.word_bank.get_blank()
 
     def get_inputs(self):
         guess = (self.console.read("Guess a letter [a-z]: ")).lower()
@@ -21,20 +24,20 @@ class Director:
 
     def do_updates(self):
         if self.correct == True:
-            Jumper.print_parachute()
+            self.jumper.print_parachute()
         elif self.correct == False:
-            Jumper.remove_parachute()
-            Jumper.print_parachute()
+            self.jumper.remove_parachute()
+            self.jumper.print_parachute()
         else:
             self.console.write("You already guessed that letter")
             self.get_inputs()
 
     def do_outputs(self):
-        alive = Jumper.is_alive()
+        alive = self.jumper.is_alive()
         if alive == False:
             self.keep_playing = False
             self.console.write(f"Oh no he died! the word was {Word_Bank.word}")
 
         else:
-            if Word_Bank.check_win == True:
+            if self.word_bank.check_win == True:
                 self.console.write(f"You did it!")
